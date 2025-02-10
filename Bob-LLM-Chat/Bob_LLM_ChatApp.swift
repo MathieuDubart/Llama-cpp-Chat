9c91ded803bdf2ed95dde3e1770910ceb6e181ca
Mathieu Dubart <mathieu.dubart@icloud.com>
2025-02-10 20:49:11 +0100

    Initial Commit

---
+++ Bob-LLM-Chat/Bob_LLM_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bob_LLM_ChatApp.swift
+//  Bob-LLM-Chat
+//
+//  Created by Mathieu Dubart on 10/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct Bob_LLM_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bob-LLM-Chat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bob-LLM-Chat
+//
+//  Created by Mathieu Dubart on 10/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
